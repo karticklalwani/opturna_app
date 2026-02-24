17c55a92e812ed4db872efe6819db3227035bc80
Sandbox user <tools@notanumber.com>
2026-02-24 22:56:34 +0000

    Please fix the following errors: Unable to resolve module ../../assets/fonts/SpaceMono-Regular.ttf from /home/user/workspace/mobile/src/app/_layout.tsx:   None of these files exist:   * SpaceMono-Regu...

--- mobile/src/app/_layout.tsx
+++ mobile/src/app/_layout.tsx
@@ -1,6 +1,5 @@
 import "../../global.css";
 import { DarkTheme, ThemeProvider } from "@react-navigation/native";
-import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { StatusBar } from "expo-status-bar";
@@ -42,15 +41,9 @@ function RootLayoutNav() {
 }
 
 export default function RootLayout() {
-  const [loaded] = useFonts({
-    SpaceMono: require("../../assets/fonts/SpaceMono-Regular.ttf"),
-  });
-
   useEffect(() => {
-    if (loaded) SplashScreen.hideAsync();
-  }, [loaded]);
-
-  if (!loaded) return null;
+    SplashScreen.hideAsync();
+  }, []);
 
   return (
     <QueryClientProvider client={queryClient}>
